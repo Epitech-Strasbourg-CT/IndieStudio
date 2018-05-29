2e44153842afaab878e7d729b50dcdefcdfeda23
Thibaut Cornolti <thibaut@cornolti.eu>
2018-05-29 14:57:38 +0200

    rahmeh main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -142,61 +142,59 @@ int main(int argc, const char** argv)
 		printf("Press any key to play some sound, press 'q' to quit.\n");
 		engine->play2D("../../media/bell.wav");
 	}
-	while(getch() != 'q')
-	{
-		video::E_DRIVER_TYPE driverType = driverChoiceConsole();
-		if (driverType == video::EDT_COUNT)
-			return
-		IrrlichtDevice *device = createDevice(driverType,
-			core::dimension2d<u32>(1920, 1080));
-		if (device == 0)
-			device->setWindowCaption(
-				L"Irrlicht Engine - User Interface Demo");
-		device->setResizable(true);
-		video::IVideoDriver *driver = device->getVideoDriver();
-		IGUIEnvironment *env = device->getGUIEnvironment();
-		IGUISkin *skin = env->getSkin();
-		IGUIFont *font = env->getFont(
-			"../../media/fonthaettenschweiler.bmp");
-		if (font)
-			skin->setFont(font);
-		skin->setFont(env->getBuiltInFont(), EGDF_TOOLTIP);
-		env->addButton(rect<s32>(700, 340, 1220, 340 + 32), 0,
-			GUI_ID_FILE_OPEN_BUTTON, L"Play", L"Launch Game");
-		env->addButton(rect<s32>(700, 380, 1220, 380 + 32), 0,
-			GUI_ID_NEW_WINDOW_BUTTON, L"Settings", L"Settings");
-		env->addButton(rect<s32>(700, 420, 1220, 420 + 32), 0,
-			GUI_ID_QUIT_BUTTON, L"Exit", L"Exit game");
-		env->addStaticText(L"Volume Control:",
-			rect<s32>(150, 20, 350, 40), true);
-		IGUIScrollBar *scrollbar = env->addScrollBar(true,
-			rect<s32>(150, 45, 350, 60), 0,
-			GUI_ID_TRANSPARENCY_SCROLL_BAR);
-		scrollbar->setMax(255);
-		scrollbar->setPos(255);
-		setSkinTransparency(scrollbar->getPos(), env->getSkin());
-		scrollbar->setPos(
-			env->getSkin()->getColor(EGDC_WINDOW).getAlpha());
-		IGUIListBox *listbox = env->addListBox(
-			rect<s32>(50, 140, 250, 210));
-		SAppContext context;
-		context.device = device;
-		context.counter = 0;
-		context.listbox = listbox;
-		MyEventReceiver receiver(context);
-		device->setEventReceiver(&receiver);
-		env->addImage(
-			driver->getTexture("../../media/Bomberman-icon.png"),
-			position2d<int>(820, 10));
-		while (device->run() && driver)
-			if (device->isWindowActive()) {
-				driver->beginScene(true, true,
-					SColor(0, 200, 200, 200));
-				env->drawAll();
-				driver->endScene();
-			}
-		device->drop();
-	}
+	while(getch() != 'q');
+	video::E_DRIVER_TYPE driverType = driverChoiceConsole();
+	if (driverType == video::EDT_COUNT)
+		return (1);
+	IrrlichtDevice *device = createDevice(driverType,
+		core::dimension2d<u32>(1920, 1080));
+	if (device == 0)
+		device->setWindowCaption(
+			L"Irrlicht Engine - User Interface Demo");
+	device->setResizable(true);
+	video::IVideoDriver *driver = device->getVideoDriver();
+	IGUIEnvironment *env = device->getGUIEnvironment();
+	IGUISkin *skin = env->getSkin();
+	IGUIFont *font = env->getFont(
+		"../../media/fonthaettenschweiler.bmp");
+	if (font)
+		skin->setFont(font);
+	skin->setFont(env->getBuiltInFont(), EGDF_TOOLTIP);
+	env->addButton(rect<s32>(700, 340, 1220, 340 + 32), 0,
+		GUI_ID_FILE_OPEN_BUTTON, L"Play", L"Launch Game");
+	env->addButton(rect<s32>(700, 380, 1220, 380 + 32), 0,
+		GUI_ID_NEW_WINDOW_BUTTON, L"Settings", L"Settings");
+	env->addButton(rect<s32>(700, 420, 1220, 420 + 32), 0,
+		GUI_ID_QUIT_BUTTON, L"Exit", L"Exit game");
+	env->addStaticText(L"Volume Control:",
+		rect<s32>(150, 20, 350, 40), true);
+	IGUIScrollBar *scrollbar = env->addScrollBar(true,
+		rect<s32>(150, 45, 350, 60), 0,
+		GUI_ID_TRANSPARENCY_SCROLL_BAR);
+	scrollbar->setMax(255);
+	scrollbar->setPos(255);
+	setSkinTransparency(scrollbar->getPos(), env->getSkin());
+	scrollbar->setPos(
+		env->getSkin()->getColor(EGDC_WINDOW).getAlpha());
+	IGUIListBox *listbox = env->addListBox(
+		rect<s32>(50, 140, 250, 210));
+	SAppContext context;
+	context.device = device;
+	context.counter = 0;
+	context.listbox = listbox;
+	MyEventReceiver receiver(context);
+	device->setEventReceiver(&receiver);
+	env->addImage(
+		driver->getTexture("../../media/Bomberman-icon.png"),
+		position2d<int>(820, 10));
+	while (device->run() && driver)
+		if (device->isWindowActive()) {
+			driver->beginScene(true, true,
+				SColor(0, 200, 200, 200));
+			env->drawAll();
+			driver->endScene();
+		}
+	device->drop();
 	engine->drop();
 	return 0;
 }
