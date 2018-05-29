d55266f6514aa43181702392ad473c169f0036fa
Thibaut Cornolti <thibaut@cornolti.eu>
2018-05-29 16:06:03 +0200

    factory

---
+++ src/EntityFactory.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** indie
+*/
+
+#include "EntityFactory.hpp"
+
+EntityFactory::EntityFactory()
+{
+}
+
+EntityFactory::~EntityFactory()
+{
+}
+
+std::unique_ptr<IEntity> EntityFactory::createEntity(const std::string &name)
+{
+	if (_fac.find(name) != _fac.end()) {
+		return _fac.at(name)();
+	} else
+		throw std::runtime_error("entity not implemented yet");
+}
---
+++ src/EntityFactory.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** indie
+*/
+
+#pragma once
+
+#include <algorithm>
+#include <map>
+#include <memory>
+
+#include "IEntity.hpp"
+
+class EntityFactory {
+public:
+	EntityFactory();
+	~EntityFactory();
+
+	std::unique_ptr<IEntity> createEntity(const std::string &);
+
+protected:
+private:
+	std::map<std::string, std::function<std::unique_ptr<IEntity>()>> _fac =
+		{};
+};
