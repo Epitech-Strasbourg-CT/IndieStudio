1cde4d394b003e09dc05335f895307ecf14004d2
Arthur Knoepflin <arthur.knoepflin@epitech.eu>
2018-05-30 16:58:39 +0200

    [+] Time

---
+++ src/Time.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** Project
+** File description:
+** Time.cpp
+*/
+
+#include <chrono>
+#include "Time.hpp"
+
+size_t Time::timestamp()
+{
+	return static_cast<size_t>(
+	std::chrono::duration_cast<std::chrono::milliseconds>
+	(std::chrono::system_clock::now().time_since_epoch()).count());
+}
