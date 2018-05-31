96b9673138d181ec6fd6041bf31020df3106ad69
Bastien <bastien.rinck@epitech.eu>
2018-05-31 16:58:18 +0200

    MenuStates

---
+++ include/States/MenuState.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2018
+** bomberman
+** File description:
+**
+*/
+#include <cstdio>
+#include <unordered_map>
+#include <functional>
+#include <irrTypes.h>
+#include "AState.hpp"
+
+#ifndef BOMBERMAN_MENUSTATE_HPP
+#define BOMBERMAN_MENUSTATE_HPP
+
+class MenuState : public AState {
+public:
+	void load() override;
+
+	void unload() override;
+
+	void update() override;
+
+private:
+	static const std::unordered_map<irr::s32, std::function<void(irr::s32, MenuState *)>> _assets;
+};
+
+#endif //BOMBERMAN_MENUSTATE_HPP
