752f67e223241eb733d04eef553758f313136142
Thibaut Cornolti <thibaut@cornolti.eu>
2018-06-05 15:37:32 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,55 @@
 ** File description:
 **
 */
-#include "../include/Singletons/StateMachine.hpp"
-#include "../include/States/SplashState.hpp"
-#include "../include/Singletons/IrrManager.hpp"
-#include "../include/Singletons/EventReceiver.hpp"
+// #include "../include/Singletons/StateMachine.hpp"
+// #include "../include/States/SplashState.hpp"
+// #include "../include/Singletons/IrrManager.hpp"
+// #include "../include/Singletons/EventReceiver.hpp"
 
-int main()
-{
-	auto shared = AStateShare();
-	auto splash = new SplashState(shared);
-	StateMachine::getInstance().push(splash, false);
-	auto device = IrrManager::getInstance().getDevice();
-	device->setEventReceiver(&EventReceiver::getInstance());
-	return StateMachine::getInstance().start();
-}
+// int main()
+// {
+// 	auto shared = AStateShare();
+// 	auto splash = new SplashState(shared);
+// 	StateMachine::getInstance().push(splash, false);
+// 	auto device = IrrManager::getInstance().getDevice();
+// 	device->setEventReceiver(&EventReceiver::getInstance());
+// 	return StateMachine::getInstance().start();
+// }
 
-/*#include <iostream>
+#include <iostream>
 #include <irrKlang.h>
+#include "../include/Game/SaveManager.hpp"
 #pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
 
 int main(int argc, const char **argv)
 {
-    irrklang::ISoundEngine *engine = irrklang::createIrrKlangDevice();
-    if (!engine)
-        return 1; // could not start engine
+	// SaveManager sm;
+	// EntitiesMap sixsixdouzedouze;
+	// AEntity bite;
+	// AEntity teub;
+	// sixsixdouzedouze.insert(bite);
+	// sixsixdouzedouze.insert(teub);
+	// for (auto *n : sixsixdouzedouze.getList())
+	// 	std::cout << *n;
+	// sm.save(sixsixdouzedouze, "test.save");
 
-    engine->play2D("assets/sounds/ophelia.mp3", true); // play some mp3 file, looped
+	// SaveManager sm2;
+	// EntitiesMap sixsixdouzedouze2;
+	// sm.load(sixsixdouzedouze2, "test.save");
+	// std::cout << std::endl;
+	// std::cout << std::endl;
+	// for (auto *n : sixsixdouzedouze2.getList())
+	// 	std::cout << *n;
+	
+	irrklang::ISoundEngine *engine = irrklang::createIrrKlangDevice();
+	if (!engine)
+		return 1; // could not start engine
 
-    std::cin.get(); // wait until user presses a key
+	engine->play2D("assets/sounds/ophelia.mp3",
+		true); // play some mp3 file, looped
 
-    engine->drop(); // delete engine
-    return 0;
-}*/
+	std::cin.get(); // wait until user presses a key
+
+	engine->drop(); // delete engine
+	return 0;
+}
